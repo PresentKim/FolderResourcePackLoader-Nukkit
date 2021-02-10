032ad98c3b159034ee4a29cb9a8e008e8a7d3245
PresentKim <debe3721@gmail.com>
2021-02-10 23:45:58 +0900

    Initial commit

---
+++ src/main/kotlin/kim/present/template/maven/Main.kt
@@ -0,0 +1,9 @@
+package kim.present.template.maven
+
+import cn.nukkit.plugin.PluginBase
+
+class Main : PluginBase() {
+    override fun onEnable() {
+        logger.info("Template plugin enabled!")
+    }
+}
